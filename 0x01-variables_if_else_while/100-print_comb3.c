7cff7c9bba99e96f6833c411559fd66036b02ebe
root <belloaugustine@gmail.com>
2022-09-09 20:12:04 -0700

    print comm

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,7 +7,6 @@
 int main(void)
 {
 	int i, j;
-
 	for (i = 0; i <= 9; i++)
 	{
 	for (j = 1; j <= 9; j++)
@@ -16,13 +15,13 @@ int main(void)
 	{
 		putchar(i + '0');
 		putchar(j + '0');
-																	if (i != 8)
+		if (i != 8)
 	{
 		putchar(',');
 		putchar(' ');
 	}
-	
-	}																}
+	}
+	}
 	}
 		putchar('\n');
 	return (0);
