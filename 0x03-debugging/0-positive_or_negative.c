459a7d35806a158fadf7311b3e15a497e42a89df
root <belloaugustine@gmail.com>
2022-09-15 15:36:04 -0700

    main file

--- 0x03-debugging/0-positive_or_negative.c
+++ 0x03-debugging/0-positive_or_negative.c
@@ -15,9 +15,12 @@ void positive_or_negative(int i)
 
 		printf("%d is positive \n", n);
 
-	else if (n == 0)
+	if (n == 0)
 
 		printf("%d is zero\n", n);
-	else
+	if (n < 0)
+{
 		printf("%d is negative\n", n);
+}
+return (0);
 }
