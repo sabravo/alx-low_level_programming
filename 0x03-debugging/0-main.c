3e441ec4e4a2ee91c328c64c7e80f69f9d6ead44
root <belloaugustine@gmail.com>
2022-09-15 20:51:28 -0700

    firs main commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,7 @@
 #include "main.h"
-
 /**
- * main - tests function that prints if integer is positive or negative
- * Return: 0
+ *main - prints positive or negative integer
+ *Return: 0
  */
 
 int main(void)
