798ced9a629c1dc311aca18336b7bbdb3fb8acda
root <belloaugustine@gmail.com>
2022-09-19 17:44:29 -0700

    function that reverses a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -20,10 +20,9 @@ void rev_string(char *s)
 	while (s[length1] != '\0')
 		length1++;
 
-	length2 = length1 - 1
+	length2 = length1 - 1;
 	for (i = 0; i < length1 / 2;  i++)
 	{
-		"samalx" "xlamas"
 		tmp = s[i];
 		s[i] = s[length2];
 		s[length2] = tmp;
