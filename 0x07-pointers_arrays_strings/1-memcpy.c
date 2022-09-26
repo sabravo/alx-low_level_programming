9cd0bcaf8ecbc2fef67ccb6aa7ec24d004b835bd
root <belloaugustine@gmail.com>
2022-09-26 15:40:12 -0700

    a function that copies memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+/*
+ * File: 1-memcpy.c
+ * Auth: Bello Augustine
+ */
+
+/**
+ *_memcpy - copies n bytes from source memory to destination
+ *@dest: destination to write to
+ *@src: source to copy
+ *@n: number of bytes to write
+ *
+ *Return: pointer to dest
+ */
+char *_memcpy(char *dest, char *src, int n)
+{
+	char *ptr = dest;
+
+	while (n--)
+	*ptr++ = *src++;
+
+	return (dest);
+}
