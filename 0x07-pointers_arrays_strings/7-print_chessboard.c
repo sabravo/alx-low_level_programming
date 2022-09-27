ca70d40f54af52eebccbc5bf577795be6fa1af4b
root <belloaugustine@gmail.com>
2022-09-26 17:15:33 -0700

    fix

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,7 +2,6 @@
 
 /**
  *print_chessboard - prints a given 2d array chess board of chars
- *
  *@a: 2d array containing board
  *
  *Return: void
@@ -14,7 +13,9 @@ void print_chessboard(char (*a)[8])
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
+
 			_putchar(a[i][j]);
+
 		_putchar('\n');
 	}
 }
