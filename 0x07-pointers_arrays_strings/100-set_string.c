cc8ed35a633613aa693456d1db83b4cda2f9f75b
root <belloaugustine@gmail.com>
2022-09-26 18:02:55 -0700

    fix

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * set_string - sets the value of a pointer to a char
- * @s: double pointer
- * @to: pointer
+ *set_string - sets the value of a pointer to a char
+ *@s: double pointer
+ *@to: pointer
  *
  */
 
