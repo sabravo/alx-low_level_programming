122abfda5862d157cd4f82e25aa2abc16693ae38
root <belloaugustine@gmail.com>
2022-09-29 16:21:23 -0700

    a function that prints a string, followed by a new linea

---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+# this is Recursion Problem
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <string.h>
+
+int _putchar(char c);
+void _print_rev_recursion(char *s);
+
+#endif /*MAIN_H*/
